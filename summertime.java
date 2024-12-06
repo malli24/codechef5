500435250f11cb5013279c659a0cef688c03b678
malli24 <malleswari82006@gmail.com>
2024-12-06 11:08:20 +0530

    Add files via upload

---
+++ summertime.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+class summertime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner S=new Scanner(System.in);
+	    int n=S.nextInt();
+	    if(n>35)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+
+	}
+}
