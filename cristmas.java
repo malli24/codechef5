40c14c15cd3bc4a2707a46e854d68d0af0718be5
malli24 <malleswari82006@gmail.com>
2024-12-06 11:12:05 +0530

    Add files via upload

---
+++ cristmas.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+class cristmas
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner S=new Scanner(System.in);
+	    int n=S.nextInt();
+	    if(n==25)
+		{
+		    System.out.println("christmas");
+		}else{
+		    System.out.println("ordinary");
+		}
+
+	}
+}
