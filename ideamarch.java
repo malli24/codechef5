5f759ab0b2d788bbba23d72ebdbb5b4c7ad5ffb8
malli24 <malleswari82006@gmail.com>
2024-12-06 11:14:38 +0530

    Add files via upload

---
+++ ideamarch.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+
+class ideamarch
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner S=new Scanner(System.in);
+	    int n=S.nextInt();
+	    if(n==15)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+
+	}
+}
