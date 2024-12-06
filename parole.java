d6520d3a51c3c361b0dbc695e5485274a7fce71d
malli24 <malleswari82006@gmail.com>
2024-12-06 11:05:22 +0530

    Add files via upload

---
+++ parole.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+class parole
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner S=new Scanner(System.in);
+	    int n=S.nextInt();
+	    if(n>=7)
+		{
+		    System.out.println("yes");
+		}else{
+		    System.out.println("no");
+		}
+
+	}
+}
+
+
